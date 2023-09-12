a295efaef0b6f1a134c6cd7a6995f5dc78c609f6
LB123658 <79328251+LB123658@users.noreply.github.com>
2023-09-11 21:11:06 -0500

    update

--- document/app.js
+++ document/app.js
@@ -38,6 +38,35 @@ var file = {
     docName: null,
     content: null
 }
+
+
+    // FOR WEB APP INSTALL
+window.addEventListener("load", () => {
+    registerSW();
+  });
+  async function registerSW() {
+    if ("serviceWorker" in navigator) {
+      try {
+        await navigator.serviceWorker.register("serviceworker.js");
+      } catch (e) {
+        console.log("SW registration failed");
+      }
+    }
+  }
+  beforeInstallPrompt = null;
+  window.addEventListener("beforeinstallprompt", eventHandler, errorHandler);
+  function eventHandler(event) {
+    beforeInstallPrompt = event;
+  }
+  function errorHandler(event) {
+    console.log("error: " + event);
+  }
+  function install() {
+    if (beforeInstallPrompt) beforeInstallPrompt.prompt();
+  }
+
+
+
     // "<URL> #/ USERNAME / FILE / FOLDER / FILE_NUMBER" FORMAT
 var filePath = window.location.hash.split("/")[1];
     // GET USER NAME FROM URL
